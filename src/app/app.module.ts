8ca4d5ccc9323264a31ee4d706fec31e3b221312
Ryan Schrope <r_schrope@yahoo.com>
2020-04-16 13:39:12 -0400

    search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireDatabaseModule } from "@angular/fire/database";
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { HomepageComponent } from './homepage/homepage.component';
+import { StartFinishComponent } from './start-finish/start-finish.component';
 
 @NgModule({
   imports: [
@@ -31,7 +32,8 @@ import { HomepageComponent } from './homepage/homepage.component';
     AppComponent,
     RegisterComponent,
     LoginComponent,
-    HomepageComponent
+    HomepageComponent,
+    StartFinishComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
